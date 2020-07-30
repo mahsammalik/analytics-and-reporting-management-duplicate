906057a1b412c6ff43a61608b472335a5dab2bae
mohabashraf <mohabashraf466@gmail.com>
2020-07-30 06:33:08 +0200

    error handling

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -5,6 +5,15 @@ export default (app) => {
 
   // POST ROUTES
   // server.get(`/api/post`, PostController.getAll);
+ /**
+ * @swagger
+ * /:
+ *  get:
+ *    description: Use to request all customers
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.post(`/api/post`, CustomerController.insert);
   //server.put(`/api/post/:id`, PostController.update);
   //server.delete(`/api/post/:id`, PostController.delete);
--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 import logger from './util/logger';
 global.logger = logger;
 const PDFDocument = require('pdfkit');
-const fs = require('fs');
-
 import express from 'express';
 import bodyParser from 'body-parser';
-import dbConnection from './util/dbConnection';
 import routes from './api/routes/routes';
 import middleware from './api/middlewares/testMiddleware';
 import Cache from './util/cache';
 import Subscriber from './services/subscriberService';
 import path from 'path';
-import responseCodeHdler from './util/responseCodeHandler';
-import DatabaseConn from './util/responseCodeHandler';
 import DB2_Connection from './util/DB2Connection';
 import EmailHandler from './util/EmailHandler';
 const { Base64Encode } = require('base64-stream');
@@ -31,34 +26,22 @@ global.imageDIR = imagePath;
 app.use(express.static(imagePath));
 
 
-
 // ********* SAMPLE CODE TESTING
 const subscriber = new Subscriber();
 subscriber.setConsumer();
-app.get('/', async (req, res) => {
-  const dump = await DB2_Connection.getValue('1030', '2020-10-01', '2020-02-01');
-  console.log("return " + dump);
-  res.send(dump);
 
-});
-
-app.get('/generatePDF', async function (req, res) {
+app.get('/api/v1/reports/statement', async function (req, res) {
+  if(req.query.start_date == undefined) res.status(404).send('Missing Start Date');
   var myDoc = new PDFDocument({ bufferPages: true });
   var finalString = ''; // contains the base64 string
     let buffers = [];
     const data = await DB2_Connection.getValue('1030', '2020-10-01', '2020-02-01');
     if (req.query.request == 'Download') {
-      var myDoc = new PDFDocument({ bufferPages: true });
-
-    myDoc.on('data', buffers.push.bind(buffers));
-    myDoc.on('end', () => {
-       let pdfData = Buffer.concat(buffers);
-       res.writeHead(200, {
-        'Content-Length': Buffer.byteLength(pdfData),
-        'Content-Type': 'application/pdf',
-        'Content-disposition': 'attachment;filename=output.pdf',
-      })
-        .end(pdfData);
+      myDoc = new PDFDocument({ bufferPages: true });
+      myDoc.on('data', buffers.push.bind(buffers));
+      let pdfData;
+     myDoc.on('end', () => {
+       pdfData = Buffer.concat(buffers);
     });
   }else if (req.query.request == 'Email'){
     myDoc.pipe(new Base64Encode());
@@ -71,27 +54,18 @@ app.get('/generatePDF', async function (req, res) {
       EmailHandler.sendEmail("","", '', finalString);
       res.json("the mail send succesful");
   });
-  }
- 
+  } 
     myDoc.font('Times-Roman')
       .fontSize(12)
       .text(data);
     myDoc.end();
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(pdfData),
+      'Content-Type': 'application/pdf',
+      'Content-disposition': 'attachment;filename=Account.pdf',
+    })
+      .end(pdfData);
   
-});
-
-
-
-app.get('/getResponse', async (req, res) => {
-  res.sendFile( path.resolve(imageDIR+ 'output12.pdf'), {
-    'Content-Length': Buffer.byteLength(pdfData),
-    'Content-Type': 'application/pdf',
-    'Content-disposition': 'attachment;filename='+imageDIR + 'output'+ 12 + '.pdf',
-  });
-  // logger.info(req.logRequestTime);
-  // responseCodeHdler.getResponseCode("ab-T12");
-  // res.send('Response Code Handler Called');
-
 });
 
 app.get('/put', async (req, res) => {
@@ -100,7 +74,6 @@ app.get('/put', async (req, res) => {
   res.send('value inserted in the data cache');
 
 });
-
 
 app.get('/get', async (req, res) => {
   logger.info(req.logRequestTime);
