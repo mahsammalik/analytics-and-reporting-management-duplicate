4f3d923b5f2581125e5c20243be2bbc817712579
mohabashraf <mohabashraf466@gmail.com>
2020-07-30 06:58:12 +0200

    refactor download pdf

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import Subscriber from './services/subscriberService';
 import path from 'path';
 import DB2_Connection from './util/DB2Connection';
 import EmailHandler from './util/EmailHandler';
+import { downloadProcess } from './downloadProcess';
 const { Base64Encode } = require('base64-stream');
 
 console.log('printing webserver value' + config.mongodb.host);
@@ -31,18 +32,22 @@ const subscriber = new Subscriber();
 subscriber.setConsumer();
 
 app.get('/api/v1/reports/statement', async function (req, res) {
-  if(req.query.start_date == undefined) res.status(404).send('Missing Start Date');
+
+  // if(req.query.start_date == undefined) res.status(404).json('Missing Start Date');
+  // if(req.query.end_date == undefined) res.status(404).json('Missing End Date');
+  // if(req.query.msisdn == undefined) res.status(404).json('Missing user\'s mobile number');
+  // if(req.query.request !== 'Email' || req.query.request !== 'Download' ) res.status(404)
+  // .send('Please send the request with either Download or Email requirement');
+  // if(req.query.request == undefined) res.status(404).json('Missing request requirement');
+
   var myDoc = new PDFDocument({ bufferPages: true });
   var finalString = ''; // contains the base64 string
     let buffers = [];
     const data = await DB2_Connection.getValue('1030', '2020-10-01', '2020-02-01');
+    let pdfData;
+
     if (req.query.request == 'Download') {
-      myDoc = new PDFDocument({ bufferPages: true });
-      myDoc.on('data', buffers.push.bind(buffers));
-      let pdfData;
-     myDoc.on('end', () => {
-       pdfData = Buffer.concat(buffers);
-    });
+      ({ myDoc, pdfData } = downloadProcess(myDoc, buffers, pdfData, res));
   }else if (req.query.request == 'Email'){
     myDoc.pipe(new Base64Encode());
     myDoc.on('data', function(chunk) {
@@ -52,20 +57,13 @@ app.get('/api/v1/reports/statement', async function (req, res) {
   myDoc.on('end', function() {
       // the stream is at its end, so push the resulting base64 string to the response
       EmailHandler.sendEmail("","", '', finalString);
-      res.json("the mail send succesful");
+      res.json("the mail send to push ");
   });
   } 
     myDoc.font('Times-Roman')
       .fontSize(12)
       .text(data);
     myDoc.end();
-    res.writeHead(200, {
-      'Content-Length': Buffer.byteLength(pdfData),
-      'Content-Type': 'application/pdf',
-      'Content-disposition': 'attachment;filename=Account.pdf',
-    })
-      .end(pdfData);
-  
 });
 
 app.get('/put', async (req, res) => {
@@ -108,3 +106,5 @@ app.get('/produceconfirm', async (req, res) => {
 routes(app);
 
 module.exports.app = app;
+
+
