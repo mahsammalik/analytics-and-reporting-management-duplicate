5747f667fea79ee0849f4cdf8d4e846ad0f9c698
Adeel Qureshi <adeelq@pk.ibm.com>
2020-08-26 02:06:11 +0500

    Updated service to make it deployable on openshift

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -1,9 +1,13 @@
-import accountStatementController from '../controllers/AccountStatementController';
+import accountStatementController from '../controllers/accountStatementController';
 import taxStatementController from '../controllers/taxStatementController';
 
 export default (app) => {
-
-  app.get('/rest/api/v1/reports/statement/account', accountStatementController.calculateAccountStatement); 
-  app.get('/rest/api/v1/reports/statement/tax', taxStatementController.calculateTaxStatement); 
-
+  app.get(
+    '/rest/api/v1/reports/statement/account',
+    accountStatementController.calculateAccountStatement
+  );
+  app.get(
+    '/rest/api/v1/reports/statement/tax',
+    taxStatementController.calculateTaxStatement
+  );
 };
