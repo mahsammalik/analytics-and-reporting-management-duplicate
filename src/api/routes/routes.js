0647d334512302b28fc0f16987f654db3e81cda0
Nadeem Abbasi <nabbasi@pk.ibm.com>
2020-09-04 15:28:50 +0500

    validation for openapi 2.0

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -2,11 +2,13 @@ import accountStatementController from '../controllers/accountStatementControlle
 import taxStatementController from '../controllers/taxStatementController';
 import { msisdnParserMW, responseCodeMW } from '../middlewares';
 import express from 'express';
+import path from 'path';
+const swaggerPath = `${path.dirname(__dirname)}/../definitions/AccountAndTaxStatement.yml`;
+// console.log(swaggerPath);
 
 const router = express.Router();
 
 router.get('/account', msisdnParserMW(), accountStatementController.calculateAccountStatement, responseCodeMW);
-
 router.get('rest/api/v1/reports/statement/tax', msisdnParserMW(), taxStatementController.calculateTaxStatement, responseCodeMW);
 
 export default router;
--- src/app.js
+++ src/app.js
@@ -7,41 +7,44 @@ import compression from 'compression';
 // import Cache from './util/cache';
 //import Subscriber from './services/subscriberService';
 import { requestLoggerMW } from './api/middlewares';
-const { OpenApiValidator } = require('express-openapi-validator');
 import path from 'path';
-
 const swaggerPath = path.join(__dirname, './definitions/AccountAndTaxStatement.yml');
-
-const openApiValidatorOptions = {
-    apiSpec: swaggerPath,
-    validateRequests: true,
-    validateResponses: false,
-    additionalProperties: false,
-    operationHandlers: true
-        // ignorePaths: /.*\/identityQuestions$/
-};
 logger.info('printing webserver value' + config.mongodb.host);
 
 logger.info('Trace message, Winston!');
+console.log(swaggerPath);
 
+
+const createMiddleware = require('@apidevtools/swagger-express-middleware');
 
 const app = express();
+
 app.use(compression());
 app.use(bodyParser.json());
 
-new OpenApiValidator(openApiValidatorOptions).install(app).then(() => {
+
+createMiddleware(swaggerPath, app, function(err, middleware) {
+    // Add all the Swagger Express Middleware, or just the ones you need.
+    // NOTE: Some of these accept optional options (omitted here for brevity)
+
+    app.use(
+        middleware.metadata(),
+        middleware.CORS(),
+        middleware.files(),
+        middleware.parseRequest(),
+        middleware.validateRequest()
+    );
+
 
     app.use('/rest/api/v1/reports/statement', router);
-
-    //Error Handler
     app.use((err, req, res, next) => {
+        console.log(err.toString());
         res.status(err.status || 500).json({
             message: err.message,
             errors: err.errors,
         });
     });
-});
 
-// app.use(requestLoggerMW());
+});
 
 module.exports.app = app;
