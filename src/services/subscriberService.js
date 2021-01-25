95fa19aacbdd803f2f7ea5d935037df650a4a55d
Muhammad Ehsan Ellahi <ehsan@Muhammads-MacBook-Pro.local>
2021-01-25 13:20:09 +0500

    chore: reverted changes

--- src/services/subscriberService.js
+++ src/services/subscriberService.js
@@ -55,6 +55,7 @@ class Subscriber {
     // config.kafkaBroker.topics.ConfirmTrans_Mobile_SOAP_USSD_Outgoing_Fail,
 
     setConsumer() {
+        console.log("SET COnsumer Called")
         this.event.addConsumerOnDataEvent(async function (msg) {
             try {
                 logger.info({ event: 'Entered function', functionName: `setConsumer in class subscriber ${msg.topic}` });
@@ -693,5 +694,5 @@ class Subscriber {
 
 }
 
-export default Subscriber;
-// export default new Subscriber();
+// export default Subscriber;
+export default new Subscriber();
