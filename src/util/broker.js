c8fd987a7f1eb170587f7bd3ad1870577a11d7e4
Mudassar Saeed <Mudassar.Saeed@ibm.com>
2021-10-13 01:21:13 +0500

    error fixed

--- src/util/broker.js
+++ src/util/broker.js
@@ -178,10 +178,7 @@ class Broker {
             // this.producer.on('ready', (arg) => {
                 this.producer.produce(topicName, partition, value);
                 // logger.debug({ event: `producer ready ${arg}` });
-            }catch (err) {
-                logger.error('Production failed: %j' + err);
-                logger.error({event: 'Error thrown - Production failed', functionName: 'Broker.produceMessage', err: { message: err.message, stack: err.stack }});
-              }
+            // });
 
 
         } catch (error) {
