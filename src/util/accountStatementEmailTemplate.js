fef867b4bc94ffd2945e3dcd337556902b119c26
Mudassar Saeed <Mudassar.Saeed@ibm.com>
2022-01-20 13:06:38 +0500

    css localized and pdf creation timeout added

--- src/util/accountStatementEmailTemplate.js
+++ src/util/accountStatementEmailTemplate.js
@@ -25,7 +25,7 @@ const accountStatementEmailTemplate = ({ title, customerName, accountNumber, sta
 		  <meta charset="UTF-8" />
 		  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+		  <link rel="stylesheet" href=url('${dirName}/css/font-awesome.min.css') />
 		
 		  <title>JazzCash</title>
 		  <style type="text/css">
