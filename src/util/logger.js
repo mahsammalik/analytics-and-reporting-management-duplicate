506d478e7bfb2200b415698a4262a848f1540b9a
alishaikh41092 <76042288+alishaikh41092@users.noreply.github.com>
2022-04-12 13:38:35 +0500

    update in logger.js

--- src/util/logger.js
+++ src/util/logger.js
@@ -5,8 +5,8 @@ import stringify from 'fast-safe-stringify';
 const { combine, timestamp, label, printf } = format;
 const PRODUCTION_LOG_LEVEL = process.env.PRODUCTION_LOG_LEVEL || 'debug';
 const MASKING_KEYS = process.env.MASKING_KEYS || "password, pin";
-require('winston-daily-rotate-file');
-const winston = require('winston');
+// require('winston-daily-rotate-file');
+// const winston = require('winston');
 
 const customFormat = printf((info) => {
     const logObj = httpContext.get('logObj') || null;
@@ -52,15 +52,15 @@ const logger = createLogger({
             level: PRODUCTION_LOG_LEVEL,
             handleExceptions: true,
         }),
-        new winston.transports.DailyRotateFile({
-			datePattern:'DD-MM-YYYY-HH', //Create new files every hour
-			zippedArchive:true, // Zipp previous files to conserve space
-			level: 'debug', // Log messages with debug or higher severity ( including info, error etc)
-			filename:'%DATE%-00-hours.log', // File name pattern, appends 1, 2, 3 etc to this name
-			dirname:'/jazzcash/data/logs', // Store logs in this path
-			maxSize:'50m', //  Create new files if size increased than 50 MB (even if hour isn't over)
-			maxFiles: '10d', // Delete files older than 10 days
-		})
+        // new winston.transports.DailyRotateFile({
+		// 	datePattern:'DD-MM-YYYY-HH', //Create new files every hour
+		// 	zippedArchive:true, // Zipp previous files to conserve space
+		// 	level: 'debug', // Log messages with debug or higher severity ( including info, error etc)
+		// 	filename:'%DATE%-00-hours.log', // File name pattern, appends 1, 2, 3 etc to this name
+		// 	dirname:'/jazzcash/data/logs', // Store logs in this path
+		// 	maxSize:'50m', //  Create new files if size increased than 50 MB (even if hour isn't over)
+		// 	maxFiles: '10d', // Delete files older than 10 days
+		// })
     ],
     exitOnError: false, // do not exit on handled exceptions
 });
