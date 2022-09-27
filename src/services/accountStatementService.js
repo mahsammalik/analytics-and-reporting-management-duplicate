16207019810181f3ab8262a8f948081b1b5a0216
Salman Haider <haider.salman@systemsltd.com>
2022-09-27 12:12:45 +0500

    Data Mapping

--- src/services/accountStatementService.js
+++ src/services/accountStatementService.js
@@ -137,7 +137,8 @@ class accountStatementService {
             //     const { data, success, message } = response;
             // if (success) {
             if (db2Data.length > 0) {
-                
+                logger.debug('DB2Data');
+                logger.debug(db2Data);
                 db2Data = db2Data.map(arr => {
                     let msisdn = arr[0];
                     let date = arr[1];
