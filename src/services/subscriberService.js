a44d395701d8fb75be97b968f2e1d3ce9fba159b
Salman Haider <haider.salman@systemsltd.com>
2022-10-11 13:02:50 +0500

    Mobile bundels trx history updated with upsert

--- src/services/subscriberService.js
+++ src/services/subscriberService.js
@@ -230,7 +230,7 @@ class Subscriber {
                 if (msg.topic === config.kafkaBroker.topics.initTrans_MobileBundle) {
                     logger.debug('*********** Init Trans Mobile Bundle *****************');
                     try {
-
+                        logger.debug(msg.value);
                         const payload = JSON.parse(msg.value);
                         payload.topic = msg.topic;
                         payload.msg_offset = msg.offset;
