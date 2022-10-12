f10a569b3c9b89eb5d5c62d1ee30c5dbecd0522e
Salman Haider <haider.salman@systemsltd.com>
2022-10-12 15:15:00 +0500

    Mobile bundels trx history updated with upsert

--- src/services/subscriberService.js
+++ src/services/subscriberService.js
@@ -313,7 +313,8 @@ class Subscriber {
                                 }
                             }
                         }
-
+                        tempData.topic = msg.topic;
+                        tempData.msg_offset = msg.offset;
                         await mobileBundleProcessor.mobileBundleConsumerProcessor(tempData);
                         //logger.debug(response);
                     } catch (error) {
