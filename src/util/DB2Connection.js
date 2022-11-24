e06ef5b6cc59fcac54cc04a1fa8ee5817db083de
hayder-salman <haider.salman@systemsltd.com>
2022-11-24 12:00:10 +0500

    Zong bundle report upsert logic reverted and targetMsisdn added

--- src/util/DB2Connection.js
+++ src/util/DB2Connection.js
@@ -177,55 +177,29 @@ class DatabaseConn {
     }
 
     if (tableName === config.reportingDBTables.MOBILE_BUNDLE_ZONG) {
-      let conn = await open(cn);
-      try {
-        // let conn = await open(cn);
-        //Check if record already exists or not
-        const stmt = conn.prepareSync(`SELECT * FROM ${schemaName}.${tableName} WHERE TRANS_ID = '${data.TID}';`);
-        logger.info("c: Select statement prepared")
-        let result = stmt.executeSync();
-        logger.info("d: Select statement executed")
-        let resultArray = result.fetchAllSync({ fetchMode: 3 }); // Fetch data in Array mode.
-        logger.info(`${schemaName}.${tableName}_selectQuery executed`);
-        const dataExixts = resultArray.length > 0;
-        logger.info(dataExixts);
-        if (data.transactionStatus == 'Pending') {
-
-          if (dataExixts) {
-            const stmt = conn.prepareSync(`UPDATE ${schemaName}.${tableName} SET AMOUNT='${data.amount}', BUNDLE_NAME='${data.bundleName}', BUNDLE_TYPE=${data.bundleType}, CHANNEL='${data.channel}', INITIATOR_MSISDN='${data.initiatorMsisdn}', NETWORK='${data.network}', TARGET_MSISDN='${data.targetMsisdn}', TRANS_DATE='${data.transactionTime}', BUNDLE_VOICE='${data.voiceMinutes}', TRANS_STATUS='Completed', TOP_NAME='${data.topic}', MSG_OFFSET='${data.msg_offset}', BUNDLE_SMS='${data.smsDetails}', BUNDLE_DATA='${data.DataDetails}', RESPONSE_CODE='${data.responseCode}', RESPONSE_DESCRIPTION='${data.responseDesc.subString(0, 50)}' WHERE TRANS_ID='${data.TID}';`);
-            stmt.executeSync();
-            stmt.closeSync();
-            //conn.close(function (err) { });
-            logger.info(`${schemaName}.${tableName}_update done`);
-          } else {
-            const stmt = conn.prepareSync(`INSERT INTO ${schemaName}.${tableName} (AMOUNT, BUNDLE_NAME, BUNDLE_TYPE, CHANNEL, INITIATOR_MSISDN, NETWORK, TARGET_MSISDN, TRANS_DATE, TRANS_ID, TOP_NAME, MSG_OFFSET, TRANS_STATUS, BUNDLE_VOICE, BUNDLE_SMS, BUNDLE_DATA, RESPONSE_CODE, RESPONSE_DESCRIPTION) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`);
-            stmt.executeSync([data.amount, data.bundleName, data.bundleType, data.channel, data.initiatorMsisdn, data.network, data.targetMsisdn, data.transactionTime, data.TID, data.topic, data.msg_offset, data.transactionStatus, data.voiceMinutes, data.smsDetails, data.DataDetails, data.responseCode, data.subString(0, 50)]);
-            stmt.closeSync();
-            //conn.close(function (err) { });
-            logger.info(`${schemaName}.${tableName}_insert done`);
-          }
+        let conn = await open(cn);
+        try {
+            // let conn = await open(cn);
+            if (data.transactionStatus == 'Pending') {
+                const stmt = conn.prepareSync(`INSERT INTO ${schemaName}.${tableName} (AMOUNT, BUNDLE_NAME, BUNDLE_TYPE, CHANNEL, INITIATOR_MSISDN, NETWORK, TARGET_MSISDN, TRANS_DATE, TRANS_ID, TOP_NAME, MSG_OFFSET, TRANS_STATUS, BUNDLE_VOICE, BUNDLE_SMS, BUNDLE_DATA, RESPONSE_CODE, RESPONSE_DESCRIPTION) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`);
+                stmt.executeSync([data.amount, data.bundleName, data.bundleType, data.channel, data.initiatorMsisdn, data.network, data.targetMsisdn, data.transactionTime, data.TID, data.topic, data.msg_offset, data.transactionStatus, data.voiceMinutes, data.smsDetails, data.DataDetails, data.responseCode, data.responseDesc]);
+                stmt.closeSync();
+                //conn.close(function (err) { });
+                logger.info(`${schemaName}.${tableName}_insert done`);
+            }
+            else if (data.transactionStatus == 'Completed') {
+                const stmt = conn.prepareSync(`UPDATE ${schemaName}.${tableName} SET TRANS_STATUS='${data.transactionStatus}', TOP_NAME='${data.topic}', MSG_OFFSET=${data.msg_offset} WHERE TRANS_ID='${data.TID}';`);
+                stmt.executeSync();
+                stmt.closeSync();
+                //conn.close(function (err) { });
+                logger.info(`${schemaName}.${tableName}_update done`);
+            }
+        } catch (err) {
+            logger.error(`${schemaName}.${tableName} database connection error` + err);
+            return await responseCodeHandler.getResponseCode(config.responseCode.useCases.accountStatement.database_connection, err);
+        } finally {
+            conn.close(function (err) { });
         }
-        else if (data.transactionStatus == 'Completed') {
-          if (dataExixts) {
-            const stmt = conn.prepareSync(`UPDATE ${schemaName}.${tableName} SET TRANS_STATUS='${data.transactionStatus}', TOP_NAME='${data.topic}', MSG_OFFSET=${data.msg_offset} WHERE TRANS_ID='${data.TID}';`);
-            stmt.executeSync();
-            stmt.closeSync();
-            //conn.close(function (err) { });
-            logger.info(`${schemaName}.${tableName}_update done`);
-          } else {
-            const stmt = conn.prepareSync(`INSERT INTO ${schemaName}.${tableName} (TRANS_ID, TRANS_STATUS, TOP_NAME, MSG_OFFSET) VALUES(?, ?, ?, ?);`);
-            stmt.executeSync([data.TID, data.transactionStatus, data.topic, data.msg_offset]);
-            stmt.closeSync();
-            //conn.close(function (err) { });
-            logger.info(`${schemaName}.${tableName}_insert done`);
-          }
-        }
-      } catch (err) {
-        logger.error(`${schemaName}.${tableName} database connection error` + err);
-        return await responseCodeHandler.getResponseCode(config.responseCode.useCases.accountStatement.database_connection, err);
-      } finally {
-        conn.close(function (err) { });
-      }
     }
 
     if (tableName === config.reportingDBTables.DONATION) {
