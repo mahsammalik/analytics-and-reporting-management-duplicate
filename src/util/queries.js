3cdcc67f37a2ff67e7b69c489a4054fcec7d9f8f
Fahad Tariq Sultan <fahad.tariqsultan@systemsltd.com>
2023-02-14 21:48:23 +0500

    build : miscellaneous helpers added

---
+++ src/util/queries.js
@@ -0,0 +1,22 @@
+const queries = {
+    merchantAccountStatment: `
+            SELECT MSISDN, TRX_DATETIME, TRX_ID, TRX_YPE, CHANNEL,
+            DESCRIPTION, AMOUNT_DEBITED, AMOUNT_CREDITED, FEE_FED , 
+            RUNNING_BALANCE, REASON_TYPE 
+            FROM statements.ACCOUNTSTATEMENT 
+            WHERE DATE(TRX_DATETIME) 
+            BETWEEN ? AND ? And MSISDN = ? OR MSISDN = ?   ;`,
+    consumerAccountStatement: `
+            SELECT MSISDN, TRX_DATETIME, TRX_ID, 
+            TRX_YPE, CHANNEL, DESCRIPTION, AMOUNT_DEBITED, 
+            AMOUNT_CREDITED, RUNNING_BALANCE, REASON_TYPE 
+            FROM statements.ACCOUNTSTATEMENT_NEW 
+            WHERE DATE(TRX_DATETIME) 
+            BETWEEN ? AND ? And MSISDN = ? OR MSISDN = ?   ;`,
+};
+
+
+
+const fetchQuery = (name) => queries[name] || null;
+
+module.exports = fetchQuery;
