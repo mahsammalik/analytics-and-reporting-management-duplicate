a0adce0b1b60d977b11a5c59f73539dda8813801
Fahad Tariq Sultan <fahad.tariqsultan@systemsltd.com>
2023-02-15 17:35:51 +0500

    FETCH RESULT

--- src/services/accountStatementService.js
+++ src/services/accountStatementService.js
@@ -158,7 +158,7 @@ class accountStatementService {
 
             logger.debug("CHECK DB2 Account Statement CSV: ", db2Data);
 
-            let header = ["Date", "Transaction ID", "Transaction Type", "Channel", "Description", "Amount Debited", "Amount Credited", "Running Balance", "Fee", "Reason Type\n"];
+            let header = ["Date", "Transaction ID", "Transaction Type", "Channel", "Description", "Amount Debited", "Amount Credited", "Fee", "Running Balance", "Reason Type\n"];
 
             header = header.join(",");
             const csvData = new Buffer.from(header + db2Data).toString("base64");
