b5ebe6c93842754c033ca50b6accd4ec5cd9d321
Fahad Tariq Sultan <fahad.tariqsultan@systemsltd.com>
2023-03-27 16:03:14 +0500

    TEST

--- src/util/PDFCreator.js
+++ src/util/PDFCreator.js
@@ -23,6 +23,7 @@ const createPDF = async(templateDetails) => {
         logger.info({ event: 'Entered function', functionName: 'createPDF' });
         logger.info({ event: 'Exited function', functionName: 'createPDF' });
         return new Promise((resolve, reject) => {
+            console.log("HERE+++++++++++++++++++++++++++++++++")
             pdf.create(templateDetails.template, options).toBuffer((err, buffer) => {
                 console.log("INSIDE CREATE PDF")
                 if (err) {
