363dbc63c7d1c9838278ff0f74db0305ce9f33b4
Salman Haider <haider.salman@systemsltd.com>
2023-03-29 14:10:53 +0500

    Static Email

--- src/util/PDFCreator.js
+++ src/util/PDFCreator.js
@@ -6,18 +6,6 @@ const options = { format: 'A4', orientation: 'portrait', type: 'pdf', timeout: '
 
 const createPDF = async(templateDetails, cb) => {
     try {
-        //Comment	for testing HTML Template
-        // const fileName = `${__dirname}/../public/${templateDetails.fileName}`;
-        // fs.writeFile(`${fileName}.html`, templateDetails.template, (err, data) => {
-        //     if (err) throw err;
-        //     logger.debug('html created');
-        //     pdf.create(templateDetails.template, options).toFile(`${fileName}.pdf`, (err, res) => {
-        //         if (err) return err;
-        //         logger.debug(res);
-        //         return res;
-        //     });
-        // // });
-
 
         logger.info({ event: 'Entered function', functionName: 'createPDF', data: templateDetails.template });
         return new Promise((resolve, reject) => {
@@ -30,8 +18,6 @@ const createPDF = async(templateDetails, cb) => {
             });
         });
         
-
-
     } catch (error) {
         logger.error({ event: 'Error throw', functionName: 'createPDF', error: { message: error.message, stack: error.stack } });
         logger.info({ event: 'Exited function', functionName: 'createPDF' });
