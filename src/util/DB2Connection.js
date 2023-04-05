eb56e5c3ec7b265a526bc8679c79517135e94f93
mahsamawan <102516837+mahsamawan@users.noreply.github.com>
2023-04-05 16:42:04 +0500

    JC-MAIN :: DB2 TABLE L0 TO L1 REPORTING CHANGES

--- src/util/DB2Connection.js
+++ src/util/DB2Connection.js
@@ -1601,33 +1601,56 @@ class DatabaseConn {
   }
 
   async addL0TOL1Reporting(payload) {
-    let conn = await getConnection();
     try {
-      logger.debug('payload L0TOL1 REPORTING data');
-      logger.debug(payload);
-      const stmt = conn.prepareSync(`INSERT INTO COMMON.L0_TO_L1_REPORT (CUSTOMER_MSISDN, CUSTOMER_PREVIOUS_STATUS, CUSTOMER_NEW_STATUS, CUSTOMER_LEVEL, CUSTOMER_CONVERSION_DATE, CUSTOMER_REGISTERATION_DATE, STATUS )
-        VALUES
-        (
-          '${payload.CUSTOMER_MSISDN}',
-          '${payload.CUSTOMER_PREVIOUS_STATUS}',
-          '${payload.CUSTOMER_NEW_STATUS}',
-          '${payload.CUSTOMER_LEVEL}',
-          '${payload.CUSTOMER_CONVERSION_DATE}',
-          '${payload.CUSTOMER_REGISTERATION_DATE}',
-          '${payload.STATUS}'
-        );`
-      );
-      stmt.executeSync();
-      stmt.closeSync();
-      logger.debug(`L0TOL1Reporting insertion done`);
-      return;
+      pool.open(cn, async (error, conn) => {
+        if (error) {
+          throw error;
+        }
+        try {
+          let query = `INSERT INTO COMMON.LOGIN_AUTH_REPORTING (CUSTOMER_MSISDN, CUSTOMER_PREVIOUS_STATUS, CUSTOMER_NEW_STATUS, CUSTOMER_LEVEL, CUSTOMER_CONVERSION_DATE, CUSTOMER_REGISTERATION_DATE, STATUS )
+                            VALUES
+                            (
+                              '${payload.CUSTOMER_MSISDN}',
+                              '${payload.CUSTOMER_PREVIOUS_STATUS}',
+                              '${payload.CUSTOMER_NEW_STATUS}',
+                              '${payload.CUSTOMER_LEVEL}',
+                              '${payload.CUSTOMER_CONVERSION_DATE}',
+                              '${payload.CUSTOMER_REGISTERATION_DATE}',
+                              '${payload.STATUS}'
+                            );`
+          await conn.query(query);
 
-    } catch (err) {
-      logger.error('Database connection error' + err);
-      return ;
-    } finally {
-      conn.close(function (err) { });
-      return
+          logger.info({
+            event: '******  DB2 Insertion was successful  ******',
+            functionName: 'DB2Connection.addL0TOL1Reporting',
+            data: query
+          });
+        } catch (err) {
+          logger.error({
+            event: `database connection error` + err,
+            functionName: 'DB2Connection.addL0TOL1Reporting',
+          });
+        } finally {
+          conn.close();
+          logger.debug({
+            event: 'Finally block - connection closed',
+            functionName: 'DB2Connection.addL0TOL1Reporting'
+          });
+        }
+
+
+      });
+    }
+    catch (error) {
+      logger.info({
+        event: '***** Error *****',
+        functionName: 'DB2Connection.report',
+        data,
+        error: {
+          message: error?.message,
+          stack: error?.stack
+        }
+      });
     }
   }
 
